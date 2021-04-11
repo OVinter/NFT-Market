6d642eec81055e92c95c46467b7bcc9adc236a9c
OVinter <ovidiu.vinter99@gmail.com>
2021-04-11 16:51:13 +0300

    NftService + NftController

--- NFT/src/main/java/com/Market/NFT/NftPackage/NftController.java
+++ NFT/src/main/java/com/Market/NFT/NftPackage/NftController.java
@@ -2,7 +2,11 @@ package com.Market.NFT.NftPackage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "/api/nfts")
@@ -13,5 +17,15 @@ public class NftController {
     @Autowired
     public NftController(NftService nftService) {
         this.nftService = nftService;
+    }
+
+    @GetMapping
+    public List<Nft> getAllNfts() {
+      return nftService.getAllNfts();
+    }
+
+    @GetMapping("{id}")
+    public Nft getProduct(@PathVariable Long id) {
+      return nftService.getNft(id);
     }
 }
