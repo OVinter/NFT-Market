f85f4257ba4cb83071f268adc778b6d59e63c7c9
Rus-Laviniu-Marius <ruslaviniumarius@gmail.com>
2021-05-14 09:25:42 +0300

    Added mail properties

--- NFT/src/main/java/com/Market/NFT/NftApplication.java
+++ NFT/src/main/java/com/Market/NFT/NftApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class NftApplication {
 
 	public static void main(String[] args) {
