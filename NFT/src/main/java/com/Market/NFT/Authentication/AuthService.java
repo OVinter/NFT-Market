6930ba7fe09150dc2c4c974e32837abfb6de2c56
Rus-Laviniu-Marius <ruslaviniumarius@gmail.com>
2021-05-14 09:32:40 +0300

    Added user registration feature

---
+++ NFT/src/main/java/com/Market/NFT/Authentication/AuthService.java
@@ -0,0 +1,50 @@
+package com.Market.NFT.Authentication;
+
+import com.Market.NFT.DTO.RegisterRequest;
+import com.Market.NFT.UserPackage.User;
+import com.Market.NFT.UserPackage.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final MailService mailService;
+
+    //@Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUserName(registerRequest.getUserName());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setEmail(registerRequest.getEmail());
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        String token = generateVerificationToken(user);
+        mailService.sendMail(new NotificationEmail("Please activate your account",
+                user.getEmail(), "Thank you, " + "please click on the below url to activate your account : " +
+                "http://localhost:8080/api/auth/accountVerification/" + token));
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+}
